df1eeb6b263e3f25470a8365bc5926632a3fe021
Ethan Glover <hello@ethang.email>
2023-05-11 20:39:32 -0500

    Init

---
+++ src/app/(components)/animated-text.tsx
@@ -0,0 +1,57 @@
+'use client';
+import { useAnimationInterval } from '@ethang/hooks/use-animation-interval';
+import classNames from 'classnames';
+import type { JSX } from 'react';
+import { useState } from 'react';
+
+import styles from './animated-text.module.css';
+
+const words = ['Plan', 'Create', 'Share'];
+
+export function AnimatedText(): JSX.Element {
+  const [currentWord, setCurrentWord] = useState(0);
+
+  const handleSetcurrentWord = (): void => {
+    setCurrentWord(currentWord_ => {
+      if (currentWord_ === 2) {
+        return 0;
+      }
+
+      return currentWord_ + 1;
+    });
+  };
+
+  useAnimationInterval(2000, handleSetcurrentWord);
+
+  return (
+    <h1
+      className={classNames(
+        'text-3xl font-semibold',
+        styles['text-anim--slide'],
+      )}
+    >
+      <span
+        className={classNames(
+          'relative inline-block py-[0.1em] px-0',
+          styles['text-anim__wrapper'],
+        )}
+      >
+        {words.map((word, index) => {
+          return (
+            <i
+              key={word}
+              className={classNames({
+                'not-italic': true,
+                [styles['text-anim__word']]: true,
+                [styles['text-anim__word--in']]: currentWord === index,
+              })}
+            >
+              {words[currentWord]}
+            </i>
+          );
+        })}
+      </span>{' '}
+      your path to learn <span className="underline">anything</span>.
+    </h1>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        marquee: 'marquee 3s linear infinite'
       },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateY(0%)' },
+          '100%': { transform: 'translateY(100%)' }
+        }
+      }
     },
   },
   plugins: [],
