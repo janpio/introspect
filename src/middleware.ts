2bba19fe1e9c76eb2b122b0adb337f117ed7548f
Ethan Glover <hello@ethang.email>
2023-05-14 13:55:12 -0500

    Add list route to public routes in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,13 @@ import { NextResponse } from 'next/server';
 
 export default authMiddleware({
   afterAuth(auth, request) {
-    if (
-      !auth.userId &&
-      !auth.isPublicRoute &&
-      !request.nextUrl.pathname.startsWith('/list')
-    ) {
+    if (!auth.userId && !auth.isPublicRoute) {
       const landingPage = new URL('/landing', request.url);
       landingPage.searchParams.set('redirect_url', request.url);
       return NextResponse.redirect(landingPage);
     }
   },
-  publicRoutes: ['/landing'],
+  publicRoutes: ['/landing', '/list/(.*)'],
 });
 
 export const config = {
