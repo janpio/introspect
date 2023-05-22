d3a496f407601417578a87f39ef75ca9049f1167
Ethan Glover <hello@ethang.email>
2023-05-21 23:18:36 -0500

    Refactor list page to separate data fetching logic into its own file.

---
+++ src/app/(routes)/list/[id]/data.ts
@@ -0,0 +1,59 @@
+import { currentUser } from '@clerk/nextjs';
+
+import { prisma } from '../../../../prisma/database';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const getListData = async (listId: string) => {
+  const user = await currentUser();
+
+  const list = await prisma.learningList.findUnique({
+    select: {
+      createdAt: true,
+      creator: {
+        select: {
+          clerkId: true,
+          profileImageUrl: true,
+          username: true,
+        },
+      },
+      favoritedBy: {
+        select: { id: true },
+      },
+      id: true,
+      learningListMaterial: {
+        orderBy: { order: 'asc' },
+        select: {
+          id: true,
+          learningMaterial: {
+            select: {
+              completedBy: {
+                select: { id: true },
+                where: {
+                  clerkId: user?.id,
+                },
+              },
+              id: true,
+              instructors: true,
+              links: {
+                select: {
+                  id: true,
+                  url: true,
+                },
+              },
+              name: true,
+              publisherName: true,
+            },
+          },
+          order: true,
+        },
+      },
+      name: true,
+      updatedAt: true,
+    },
+    where: {
+      id: listId,
+    },
+  });
+
+  return { list, user };
+};
--- src/app/(routes)/list/[id]/page.tsx
+++ src/app/(routes)/list/[id]/page.tsx
@@ -1,11 +1,10 @@
-import { currentUser } from '@clerk/nextjs';
 import { DateTime } from 'luxon';
 import type { JSX } from 'react';
 
-import { prisma } from '../../../../prisma/database';
 import { ListCard } from '../../../(components)/list-card';
 import { CreateModal } from './(components)/create-modal';
 import { MaterialCard } from './(components)/material-card';
+import { getListData } from './data';
 
 type ListPageProperties = {
   params: {
@@ -16,55 +15,7 @@ type ListPageProperties = {
 export default async function ListPage({
   params,
 }: ListPageProperties): Promise<JSX.Element | null> {
-  const user = await currentUser();
-
-  const list = await prisma.learningList.findUnique({
-    select: {
-      createdAt: true,
-      creator: {
-        select: {
-          clerkId: true,
-          profileImageUrl: true,
-          username: true,
-        },
-      },
-      favoritedBy: {
-        select: { id: true },
-      },
-      id: true,
-      learningListMaterial: {
-        orderBy: { order: 'asc' },
-        select: {
-          learningMaterial: {
-            select: {
-              completedBy: {
-                select: { id: true },
-                where: {
-                  clerkId: user?.id,
-                },
-              },
-              id: true,
-              instructors: true,
-              links: {
-                select: {
-                  id: true,
-                  url: true,
-                },
-              },
-              name: true,
-              publisherName: true,
-            },
-          },
-          order: true,
-        },
-      },
-      name: true,
-      updatedAt: true,
-    },
-    where: {
-      id: params.id,
-    },
-  });
+  const { user, list } = await getListData(params.id);
 
   if (!list) {
     return null;
