f07d005cf8c7d26f269ce9fa8736762a03502aa8
Ethan Glover <hello@ethang.email>
2023-05-25 12:57:03 -0500

    Remove error creation and display from error pages

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,29 +2,7 @@
 import Link from 'next/link';
 import type { JSX } from 'react';
 
-import { ROOT_URL } from '../util/constants';
-import { zodFetch } from '../util/zod';
-import {
-  createErrorBodySchema,
-  createErrorReturnSchema,
-} from './api/create-error/types';
-
-type ErrorProperties = {
-  error: Error;
-};
-
-export default async function ErrorPage({
-  error,
-}: ErrorProperties): Promise<JSX.Element> {
-  const createdError = await zodFetch(
-    createErrorReturnSchema,
-    `${ROOT_URL}/api/create-error`,
-    {
-      body: JSON.stringify(createErrorBodySchema.parse(error)),
-      method: 'POST',
-    },
-  );
-
+export default async function ErrorPage(): Promise<JSX.Element> {
   return (
     <main>
       <div className="text-center">
@@ -32,7 +10,6 @@ export default async function ErrorPage({
           Error!
         </h1>
         <p className="mt-6 text-base leading-7">Something went wrong!</p>
-        <p className="mt-6 text-base leading-7">Error Id: {createdError.id}</p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <Link
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,36 +1,18 @@
 'use client';
 import type { JSX } from 'react';
 
-import { ROOT_URL } from '../util/constants';
-import { zodFetch } from '../util/zod';
-import {
-  createErrorBodySchema,
-  createErrorReturnSchema,
-} from './api/create-error/types';
-
 type GlobalErrorProperties = {
   error: Error;
   reset: () => void;
 };
 
 export default async function GlobalError({
-  error,
   reset,
 }: GlobalErrorProperties): Promise<JSX.Element> {
-  const createdError = await zodFetch(
-    createErrorReturnSchema,
-    `${ROOT_URL}/api/create-error`,
-    {
-      body: JSON.stringify(createErrorBodySchema.parse(error)),
-      method: 'POST',
-    },
-  );
-
   return (
     <html lang="en-US">
       <body>
         <h2>Something has gone terribly wrong!</h2>
-        <p>Error Id: {createdError.id}</p>
         <button
           type="button"
           onClick={(): void => {
